c0e622c5f453eb20a07563e0ab728580c17acc1b
etcsa044 <carlos.d.pelayes@gmail.com>
2023-06-26 11:23:18 -0300

    correccion de requisitos en el model user

--- Dao/mongo/models/user.model.js
+++ Dao/mongo/models/user.model.js
@@ -6,20 +6,16 @@ const collection = "users";
 const schema = new mongoose.Schema({
     first_name : {
         type : String,
-        required : true,
     },
     last_name : {
         type:String,
-        required: true
     },
     email : {
         type: String,
-        required: true,
         index : true
     },
     password:{
         type: String,
-        required: true,
     },
     role:{
        type:String,
